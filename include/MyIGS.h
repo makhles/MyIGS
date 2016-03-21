366ffde1633a747937b2f84e370d524ae36eb7fc
makhles <mr.lange85@gmail.com>
2016-03-21 20:08:54 -0300

    Displacement and zoom control buttons added.

--- include/MyIGS.h
+++ include/MyIGS.h
@@ -16,8 +16,16 @@ class MyIGS : public Gtk::Window {
         Gtk::Frame _objectsFrame;
         /* Widgets used in the Control Frame */
         Gtk::Box _controlBox;
+        Gtk::Box _zoomBox;
+        Gtk::Grid _dispGrid;
         Gtk::Frame _zoomFrame;
         Gtk::Frame _dispFrame;
+        Gtk::Button _zoomButtonOut;
+        Gtk::Button _zoomButtonIn;
+        Gtk::Button _dispButtonUp;
+        Gtk::Button _dispButtonDown;
+        Gtk::Button _dispButtonLeft;
+        Gtk::Button _dispButtonRight;
         /* Widgets used in the Viewport Frame */
         Gtk::Box _viewportBox;
         Canvas _canvas;
@@ -27,6 +35,12 @@ class MyIGS : public Gtk::Window {
         void createControlFrame();
         void createViewportFrame();
         void createObjectsFrame();
+        void zoom_in();
+        void zoom_out();
+        void displaceUp();
+        void displaceRight();
+        void displaceDown();
+        void displaceLeft();
     public:
         MyIGS();
         virtual ~MyIGS();
--- src/MyIGS.cpp
+++ src/MyIGS.cpp
@@ -1,8 +1,16 @@
 /* MyIGS.cpp */
 
+#include <iostream>
 #include "MyIGS.h"
 
-MyIGS::MyIGS() : _mainBox(Gtk::ORIENTATION_HORIZONTAL) {
+MyIGS::MyIGS() :
+        _mainBox(Gtk::ORIENTATION_HORIZONTAL),
+        _zoomButtonOut("Out"),
+        _zoomButtonIn("In"),
+        _dispButtonUp("Up"),
+        _dispButtonDown("Down"),
+        _dispButtonLeft("Left"),
+        _dispButtonRight("Right") {
 
     /* Main window */
     set_title("My IGS");
@@ -23,22 +31,39 @@ MyIGS::~MyIGS() {
 
 void MyIGS::createControlFrame() {
     _controlBox.set_orientation(Gtk::ORIENTATION_VERTICAL);
+    _zoomBox.set_orientation(Gtk::ORIENTATION_HORIZONTAL);
 
+    /* Zoom frame */
     _zoomFrame.set_label("Zoom");
     _zoomFrame.set_shadow_type(Gtk::SHADOW_ETCHED_OUT);
     _zoomFrame.set_border_width(5);
-    _controlBox.pack_start(_zoomFrame, Gtk::PACK_EXPAND_WIDGET, 1);
+    _zoomFrame.add(_zoomBox);
+    _controlBox.pack_start(_zoomFrame, Gtk::PACK_SHRINK, 1);
 
+    /* Zoom frame buttons */
+    _zoomButtonOut.signal_clicked().connect(sigc::mem_fun(*this, &MyIGS::zoom_out));
+    _zoomButtonIn.signal_clicked().connect(sigc::mem_fun(*this, &MyIGS::zoom_in));
+    _zoomBox.pack_start(_zoomButtonOut, Gtk::PACK_EXPAND_WIDGET, 1);
+    _zoomBox.pack_start(_zoomButtonIn, Gtk::PACK_EXPAND_WIDGET, 1);
+
+    /* Displacement Frame */
     _dispFrame.set_label("Displacement");
     _dispFrame.set_shadow_type(Gtk::SHADOW_ETCHED_OUT);
     _dispFrame.set_border_width(5);
-    _controlBox.pack_start(_dispFrame, Gtk::PACK_EXPAND_WIDGET, 1);
+    _controlBox.pack_start(_dispFrame, Gtk::PACK_SHRINK, 1);
+
+    /* Displacement buttons */
+    _dispGrid.attach(_dispButtonUp,2,1,1,1);
+    _dispGrid.attach(_dispButtonRight,3,2,1,1);
+    _dispGrid.attach(_dispButtonDown,2,3,1,1);
+    _dispGrid.attach(_dispButtonLeft,1,2,1,1);
+    _dispFrame.add(_dispGrid);
 
     _controlFrame.set_label("Control");
     _controlFrame.set_shadow_type(Gtk::SHADOW_ETCHED_OUT);
     _controlFrame.add(_controlBox);
 
-    _mainBox.pack_start(_controlFrame, Gtk::PACK_EXPAND_WIDGET, 1);
+    _mainBox.pack_start(_controlFrame, Gtk::PACK_SHRINK, 1);
 }
 
 void MyIGS::createViewportFrame() {
@@ -57,4 +82,28 @@ void MyIGS::createObjectsFrame() {
     _objectsFrame.set_label("Objects");
     _objectsFrame.set_shadow_type(Gtk::SHADOW_ETCHED_OUT);
     _mainBox.pack_start(_objectsFrame, Gtk::PACK_EXPAND_WIDGET, 1);
+}
+
+void MyIGS::zoom_in() {
+    std::cout << "Zooming in!" << std::endl;
+}
+
+void MyIGS::zoom_out() {
+    std::cout << "Zooming out!" << std::endl;
+}
+
+void MyIGS::displaceUp() {
+
+}
+
+void MyIGS::displaceRight() {
+
+}
+
+void MyIGS::displaceDown() {
+
+}
+
+void MyIGS::displaceLeft() {
+
 }
