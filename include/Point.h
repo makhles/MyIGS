ae349dd380c5757635a1e72be03fe35ef9925659
makhles <mr.lange85@gmail.com>
2016-03-25 13:51:24 -0300

    Point now works with double instead of size_t

--- include/Point.h
+++ include/Point.h
@@ -1,24 +1,20 @@
-/* Point.h
-   Copyright: Makhles Reuter Lange, 2016.
-*/
+/* Point.h */
 
 #ifndef POINT_H
 #define POINT_H
 
-#include <cstddef>              // For size_t
 #include "Shape.h"
 
 class Point : public Shape {
     friend class Line;
     private:
-        size_t _xPos;
-        size_t _yPos;
+        const double _xPos;
+        const double _yPos;
     public:
-        Point(const std::string name, const size_t &xPos, const size_t &yPos);
+        Point(const std::string name, const double &xPos, const double &yPos);
         ~Point();
-        size_t get_xPos() const {return _xPos;}
-        size_t get_yPos() const {return _yPos;}
-        void set_xPos(const size_t &xPos) {_xPos = xPos;}
-        void set_yPos(const size_t &yPos) {_yPos = yPos;}
+        const double get_xPos() const {return _xPos;}
+        const double get_yPos() const {return _yPos;}
 };
-#endif
+
+#endif  // POINT_H
--- src/Point.cpp
+++ src/Point.cpp
@@ -2,7 +2,7 @@
 
 #include "Point.h"
 
-Point::Point(const std::string name, const size_t &xPos, const size_t &yPos) :
+Point::Point(const std::string name, const double &xPos, const double &yPos) :
         Shape::Shape(name, ShapeType::POINT),
         _xPos(xPos),
         _yPos(yPos) {
