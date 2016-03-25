9e0f3cb07b83b8c9a05cbb2d315a0b05f3d51c7b
makhles <mr.lange85@gmail.com>
2016-03-25 12:01:57 -0300

    WorldWindow became a mere container for values

--- include/WorldWindow.h
+++ include/WorldWindow.h
@@ -3,30 +3,19 @@
 #ifndef WORLDWINDOW_H
 #define WORLDWINDOW_H
 
-#define XMIN 0
-#define YMIN 0
-#define XMAX 100
-#define YMAX 500
-#define TRANSLATION_OFFSET 10.0  // Displacement units
-#define DEFAULT_ZOOM 100  // Porcentage
-#define ZOOM_FACTOR 25  // Porcentage
-
 class WorldWindow {
-    private:
+    public:
+        const double XMIN = 0;
+        const double YMIN = 0;
+        const double XMAX = 100;
+        const double YMAX = 100;
         double _xMin;
         double _xMax;
         double _yMin;
         double _yMax;
-        double _zoom;
     public:
         WorldWindow();
         ~WorldWindow();
-        void translateUp();
-        void translateRight();
-        void translateDown();
-        void translateLeft();
-        void zoomIn();
-        void zoomOut();
 };
 
 #endif  // WORLDWINDOW_H
--- src/WorldWindow.cpp
+++ src/WorldWindow.cpp
@@ -3,41 +3,11 @@
 #include "WorldWindow.h"
 
 WorldWindow::WorldWindow() :
-        _xMin(XMIN),
-        _xMax(XMAX),
-        _yMin(YMIN),
-        _yMax(YMAX),
-        _zoom(DEFAULT_ZOOM) {
-
+        _xMin(WorldWindow::XMIN),
+        _xMax(WorldWindow::XMAX),
+        _yMin(WorldWindow::YMIN),
+        _yMax(WorldWindow::YMAX) {
 }
 
 WorldWindow::~WorldWindow() {
-}
-
-void WorldWindow::translateUp() {
-    _yMin += TRANSLATION_OFFSET;
-    _yMax += TRANSLATION_OFFSET;
-}
-
-void WorldWindow::translateRight() {
-    _xMin += TRANSLATION_OFFSET;
-    _xMax += TRANSLATION_OFFSET;
-}
-
-void WorldWindow::translateDown() {
-    _yMin -= TRANSLATION_OFFSET;
-    _yMax -= TRANSLATION_OFFSET;
-}
-
-void WorldWindow::translateLeft() {
-    _xMin -= TRANSLATION_OFFSET;
-    _xMax -= TRANSLATION_OFFSET;
-}
-
-void WorldWindow::zoomIn() {
-    //TODO
-}
-
-void WorldWindow::zoomOut() {
-    //TODO
 }
