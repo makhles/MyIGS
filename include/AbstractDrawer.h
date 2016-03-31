5d816386ad231c8f44f85010329ae1f80f86d64c
makhles <mr.lange85@gmail.com>
2016-03-30 21:23:02 -0300

    Created abstract class AbstractDrawer.

---
+++ include/AbstractDrawer.h
@@ -0,0 +1,15 @@
+/* AbstractDrawer */
+
+#ifndef ABSTRACT_DRAWER_H
+#define ABSTRACT_DRAWER_H
+
+class AbstractDrawer
+{
+    public:
+        virtual void draw(Point *point) = 0;
+        virtual void draw(Line *line) = 0;
+        virtual void draw(Wireframe *wireframe) = 0;
+
+};
+
+#endif  // ABSTRACT_DRAWER_H
