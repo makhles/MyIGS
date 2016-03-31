154dbdb296d62e2361471734e85b3e269bf274bf
makhles <mr.lange85@gmail.com>
2016-03-30 22:04:20 -0300

    Context properties set for each Shape in ShapeDrawer.

--- src/ShapeDrawer.cpp
+++ src/ShapeDrawer.cpp
@@ -15,15 +15,27 @@ ShapeDrawer::~ShapeDrawer() {
 }
 
 void ShapeDrawer::draw(Point *point) {
+    _cr->save();
+    _cr->set_line_cap(Cairo::LINE_CAP_ROUND);
+    _cr->set_line_width(5.0);
     this->drawCoordinates(point->getViewportCoordinates());
+    _cr->restore();
 }
 
 void ShapeDrawer::draw(Line *line) {
+    _cr->save();
+    _cr->set_line_cap(Cairo::LINE_CAP_SQUARE);
+    _cr->set_line_width(1.0);
     this->drawCoordinates(line->getViewportCoordinates());
+    _cr->restore();
 }
 
 void ShapeDrawer::draw(Wireframe *wireframe) {
+    _cr->save();
+    _cr->set_line_cap(Cairo::LINE_CAP_SQUARE);
+    _cr->set_line_width(1.0);
     this->drawCoordinates(wireframe->getViewportCoordinates());
+    _cr->restore();
 }
 
 void ShapeDrawer::drawCoordinates(const std::list<const Coord<int>*> *coordinates) {
