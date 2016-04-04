88997744250c2a2cb2ce1ff6d746ae86178466e5
makhles <mr.lange85@gmail.com>
2016-04-04 01:14:25 -0300

    TransformationType created.

---
+++ include/TransformationType.hpp
@@ -0,0 +1,15 @@
+// TransformationType.hpp
+// Authors: Leonardo Vailatti Eichstaedt
+//          Makhles Reuter Lange
+
+#ifndef TRANSFORMATION_TYPE_HPP
+#define TRANSFORMATION_TYPE_HPP
+
+enum class TransformationType
+{
+    TRANSLATION,
+    SCALING,
+    ROTATION
+};
+
+#endif  // TRANSFORMATION_TYPE_HPP
