8c2a450dcdc65294d5cfad73dc729e53c0de9a68
makhles <mr.lange85@gmail.com>
2016-04-10 17:20:21 -0300

    WindowHandler created. Initial implementation: translate_window is working.

---
+++ include/WindowHandler.hpp
@@ -0,0 +1,27 @@
+// WindowHandler.hpp
+// Authors: Leonardo Vailatti Eichstaedt
+//          Makhles Reuter Lange
+
+#ifndef WINDOW_HANDLER_HPP
+#define WINDOW_HANDLER_HPP
+
+#include "Window.hpp"
+
+// Forward declarations
+class Canvas;
+
+class WindowHandler {
+protected:
+    const double DEVICE_DISPLACEMENT = 10.0;  // Pixels
+
+    Window m_window;
+    Canvas *m_viewport;
+
+public:
+    WindowHandler() {}
+    ~WindowHandler() {}
+    void set_viewport(Canvas *canvas) { m_viewport = canvas; }
+    void translate_window(int rightOrLeft, int upOrDown);
+};
+
+#endif  // WINDOW_HANDLER_HPP
---
+++ src/WindowHandler.cpp
@@ -0,0 +1,17 @@
+// WindowHandler.cpp
+// Authors: Leonardo Vailatti Eichstaedt
+//          Makhles Reuter Lange
+
+#include "WindowHandler.hpp"
+#include "Canvas.hpp"
+
+
+
+// Right/Up = 1, Left/Down = -1, stay still = 0
+// dx is the required horizontal displacement so that the viewport
+// will be translated by DEVICE_DISPLACEMENT units (in pixels).
+void WindowHandler::translate_window(int rightOrLeft, int upOrDown) {
+    double dx = DEVICE_DISPLACEMENT * m_window.width() / m_viewport->get_viewport_width();
+    double dy = DEVICE_DISPLACEMENT * m_window.height() / m_viewport->get_viewport_height();
+    m_window.translate(rightOrLeft * dx, upOrDown * dy);
+}
