b74b4d1c058530d343c3ac47121b2434b25201e5
makhles <mr.lange85@gmail.com>
2016-04-13 19:05:22 -0300

    ObjWriter created. Nothing implemented, though.

---
+++ include/ObjWriter.hpp
@@ -0,0 +1,16 @@
+// ObjWriter.hpp
+// Authors: Leonardo Vailatti Eichstaedt
+//          Makhles Reuter Lange
+
+#ifndef OBJ_WRITER_HPP
+#define OBJ_WRITER_HPP
+
+class ObjWriter {
+private:
+    int m_lineCount;
+public:
+    ObjWriter();
+    ~ObjWriter();
+};
+
+#endif  // OBJ_WRITER_HPP
---
+++ src/ObjWriter.cpp
@@ -0,0 +1,18 @@
+// ObjWriter.cpp
+// Authors: Leonardo Vailatti Eichstaedt
+//          Makhles Reuter Lange
+
+#include "ObjWriter.hpp"
+
+ObjWriter::ObjWriter() :
+    m_lineCount(0)
+{
+
+}
+
+
+ObjWriter::~ObjWriter() {
+
+}
+
+
