a93195f1a7b7a0dc565f9cd313ccdd8f8f584c8f
makhles <mr.lange85@gmail.com>
2016-04-17 22:09:52 -0300

    Viewport rectangle created on Canvas.

--- include/Canvas.hpp
+++ include/Canvas.hpp
@@ -16,25 +16,41 @@ typedef Cairo::RefPtr<Cairo::Context> CairoCtx;
 
 class Canvas : public Gtk::DrawingArea {
 protected:
-    InterfaceController *m_controller;
-    ShapeDrawer m_drawer;
-    bool m_darkBG;
-    bool m_drawGridlines;
-
-protected:
     bool on_draw(const CairoCtx &cr) override;
+    void draw_theme(const CairoCtx &cr);
+    void draw_viewport(const CairoCtx &cr);
     void draw_center_lines(const CairoCtx &cr);
+    void draw_gridlines(const CairoCtx &cr);
     void draw_shapes(const CairoCtx &cr);
 
 public:
     Canvas();
     virtual ~Canvas();
-    double xvp_min() const { return 0; }  // temporary
-    double yvp_max() const { return this->vp_height(); }  // temporary
-    double vp_width() const;
-    double vp_height() const;
+    double xvp_min() const { return VP_XMIN; }
+    double yvp_max() const { return VP_YMAX; }
+    double vp_width() const { return VP_WIDTH; }
+    double vp_height() const { return VP_HEIGHT; }
     void set_controller(InterfaceController *controller) { m_controller = controller; }
     void invalidate();
+
+private:
+    // Canvas sizes
+    const int WIDTH = 500;
+    const int HEIGHT = 500;
+    const int OFFSET = 10;
+
+    // Vierport coordinates
+    const int VP_WIDTH = WIDTH - 2 * OFFSET;
+    const int VP_HEIGHT = HEIGHT - 2 * OFFSET;
+    const int VP_XMIN = OFFSET;
+    const int VP_YMIN = OFFSET;
+    const int VP_XMAX = VP_XMIN + VP_WIDTH;
+    const int VP_YMAX = VP_YMIN + VP_HEIGHT;
+
+    InterfaceController *m_controller;
+    ShapeDrawer m_drawer;
+    bool m_darkBG;
+    bool m_drawGridlines;
 };
 
 #endif  // CANVAS_H
--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -12,28 +12,29 @@ Canvas::Canvas() :
     m_darkBG(false),
     m_drawGridlines(false)
 {
-    set_size_request(500,500);
+    set_size_request(WIDTH, HEIGHT);
 }
 
 Canvas::~Canvas() {
 }
 
 bool Canvas::on_draw(const CairoCtx &cr) {
+    cr->save();
+    this->draw_theme(cr);
+    this->draw_viewport(cr);
     this->draw_center_lines(cr);
+
+    if (m_drawGridlines) {
+        this->draw_gridlines(cr);
+    }
+
     this->draw_shapes(cr);
+    cr->restore();
     return true;
 }
 
 
-void Canvas::draw_center_lines(const CairoCtx &cr) {
-    Gtk::Allocation allocation = get_allocation();
-    const int width = allocation.get_width();
-    const int height = allocation.get_height();
-
-    cr->save();
-    cr->set_line_width(1.0);
-
-    // Background
+void Canvas::draw_theme(const CairoCtx &cr) {
     if (m_darkBG) {
         cr->set_source_rgba(0.0, 0.0, 0.0, 0.7);  // Black background
         cr->paint();
@@ -43,61 +44,81 @@ void Canvas::draw_center_lines(const CairoCtx &cr) {
         cr->paint();
         cr->set_source_rgba(0.6, 0.6, 0.6, 1.0);
     }
+    cr->set_line_width(1.0);
+}
 
-// --------------------------------------------------------------------------------------------- //
-// ---------------------------------------- Gridlines ------------------------------------------ //
 
-    if (m_drawGridlines) {
+void Canvas::draw_viewport(const CairoCtx &cr) {
+    // Top horizontal line
+    cr->move_to(VP_XMIN, VP_YMIN);
+    cr->line_to(VP_XMAX, VP_YMIN);
 
-        int spacing = width / 20;
-        int hlines_count = height / spacing;
-        int vlines_count = width / spacing;
-        int pos = spacing;
+    // Right vertical line
+    cr->move_to(VP_XMAX, VP_YMIN);
+    cr->line_to(VP_XMAX, VP_YMAX);
 
-        std::vector<double> dashes{5.0};
-        cr->set_dash(dashes, 0.0);
+    // Bottom horizontal line
+    cr->move_to(VP_XMAX, VP_YMAX);
+    cr->line_to(VP_XMIN, VP_YMAX);
 
-        // Draw horizontal grid lines
-        for (int i = 0; i < hlines_count - 1; i++) {
-            cr->move_to(0, pos);
-            cr->line_to(width, pos);
-            pos += spacing;
-        }
+    // Left vertical line
+    cr->move_to(VP_XMIN, VP_YMAX);
+    cr->line_to(VP_XMIN, VP_YMIN);
 
-        // Draw vertical grid lines
-        pos = spacing;
-        for (int i = 0; i < vlines_count - 1; i++) {
-            cr->move_to(pos, 0);
-            cr->line_to(pos, height);
-            pos += spacing;
-        }
+    cr->stroke();
+}
 
-        cr->unset_dash();
-    }
 
-// --------------------------------------------------------------------------------------------- //
-// ------------------------------------- Viewport center --------------------------------------- //
-
+void Canvas::draw_center_lines(const CairoCtx &cr) {
     // Coordinates for the center of the window
-    int xc = width / 2;
-    int yc = height / 2;
+    int xc = VP_XMIN + VP_WIDTH / 2;
+    int yc = VP_YMIN + VP_HEIGHT / 2;
 
     // Set stroke properties for the centered axis
     cr->set_line_width(1.0);
     cr->set_source_rgba(0.0, 0.0, 0.0, 0.4);  // Light gray
 
     // Create horizontal and vertical axis centered on the screen
-    cr->move_to(0, yc);
-    cr->line_to(width, yc);
-    cr->move_to(xc, 0);
-    cr->line_to(xc, height);
+    cr->move_to(VP_XMIN, yc);
+    cr->line_to(VP_XMAX, yc);
+    cr->move_to(xc, VP_YMIN);
+    cr->line_to(xc, VP_YMAX);
 
     cr->stroke();
+}
+
+
+void Canvas::draw_gridlines(const CairoCtx &cr) {
+    int spacing = VP_WIDTH / 20;
+    int hlines_count = VP_HEIGHT / spacing;
+    int vlines_count = VP_WIDTH / spacing;
+    int pos = VP_YMIN + spacing;
+    std::vector<double> dashes{5.0};
+
+    cr->save();
+    cr->set_dash(dashes, 0.0);
+
+    // Draw horizontal grid lines
+    for (int i = 0; i < hlines_count - 1; i++) {
+        cr->move_to(VP_XMIN, pos);
+        cr->line_to(VP_XMAX, pos);
+        pos += spacing;
+    }
+
+    // Draw vertical grid lines
+    pos = VP_XMIN + spacing;
+    for (int i = 0; i < vlines_count - 1; i++) {
+        cr->move_to(pos, VP_YMIN);
+        cr->line_to(pos, VP_YMAX);
+        pos += spacing;
+    }
+
     cr->restore();
 }
 
 
 void Canvas::draw_shapes(const CairoCtx &cr) {
+    cr->set_source_rgba(0.0, 0.0, 0.0, 0.7);  // Back in black
     m_drawer.set_cairo_context(cr);
     m_controller->draw_shapes(m_drawer);
 }
@@ -110,14 +131,4 @@ void Canvas::invalidate() {
         Gdk::Rectangle r(0, 0, get_allocation().get_width(), get_allocation().get_height());
         win->invalidate_rect(r, false);
     }
-}
-
-
-double Canvas::vp_width() const {
-    return get_allocation().get_width();
-}
-
-
-double Canvas::vp_height() const {
-    return get_allocation().get_height();
 }
