7c8f051abefc6ec4033cd53f3f4828a0f9035fbc
makhles <mr.lange85@gmail.com>
2016-05-01 17:57:57 -0300

    Bezier curves can now be concatenated using the C1 continuity

--- include/CoordBox.hpp
+++ include/CoordBox.hpp
@@ -15,6 +15,10 @@
 class CoordBox : public Gtk::VBox {
 protected:
     std::vector<Gtk::Entry*> m_entries;
+    double m_x2;
+    double m_y2;
+    double m_x3;
+    double m_y3;
 
 public:
     CoordBox() {}
@@ -25,6 +29,10 @@ public:
     void add_curve();
     void add_bezier_curve();
     void add_coord();
+
+    // Signal handlers
+    bool on_x_entry_focus_out_event(GdkEventFocus*);
+    bool on_y_entry_focus_out_event(GdkEventFocus*);
 };
 
 #endif  // COORD_BOX_HPP
--- src/CoordBox.cpp
+++ src/CoordBox.cpp
@@ -2,8 +2,16 @@
 // Authors: Leonardo Vailatti Eichstaedt
 //          Makhles Reuter Lange
 
+#include <iostream>
 #include "CoordBox.hpp"
 
+// For debugging, uncomment the following define
+#define DEBUG
+#ifdef DEBUG
+#define DEBUG_MSG(str) do { std::cout << str << std::endl; } while( false )
+#else
+#define DEBUG_MSG(str) do { } while ( false )
+#endif
 
 // Add the first Bézier curve (4 points only)
 void CoordBox::add_curve() {
@@ -21,26 +29,55 @@ void CoordBox::add_bezier_curve() {
         m_entries[i]->set_sensitive(false);
     }
 
-    int last_entry_index = m_entries.size() - 1;
-    double last_x, last_y;
-    std::stringstream stream_last_x, stream_last_y;
-    stream_last_x << m_entries[last_entry_index - 1]->get_text().raw();
-    stream_last_y << m_entries[last_entry_index - 0]->get_text().raw();
-    stream_last_x >> last_x;
-    stream_last_y >> last_y;
+    int p3_index = m_entries.size() - 1;
+    std::stringstream stream_x2, stream_y2;
+    std::stringstream stream_x3, stream_y3;
 
+    stream_x2 << m_entries[p3_index - 3]->get_text().raw();
+    stream_y2 << m_entries[p3_index - 2]->get_text().raw();
+    stream_x3 << m_entries[p3_index - 1]->get_text().raw();
+    stream_y3 << m_entries[p3_index - 0]->get_text().raw();
+    stream_x2 >> m_x2;
+    stream_y2 >> m_y2;
+    stream_x3 >> m_x3;
+    stream_y3 >> m_y3;
+
+    // Add the new entries
     for (unsigned i = 0; i < 4; i++) {
         CoordBox::add_coord();
     }
 
     // The first entry of the recently added curve must have the same
     // coordinates as the last entry of the previous curve (C0)
-    m_entries[last_entry_index + 1]->set_text(std::to_string(last_x));
-    m_entries[last_entry_index + 2]->set_text(std::to_string(last_y));
+    m_entries[p3_index + 1]->set_text(std::to_string(m_x3));
+    m_entries[p3_index + 2]->set_text(std::to_string(m_y3));
 
     // Prevent user from messing this up
-    m_entries[last_entry_index + 1]->set_sensitive(false);
-    m_entries[last_entry_index + 2]->set_sensitive(false);
+    m_entries[p3_index + 1]->set_sensitive(false);
+    m_entries[p3_index + 2]->set_sensitive(false);
+
+    // Add a signal handler only if the points P2 e P3 do not reside in the same
+    // X or Y coordinate. If they do, this coordinate is fixed.
+    bool enable_out_event = true;
+
+    if (m_x3 - m_x2 == 0.0) {
+        m_entries[p3_index + 3]->set_text(std::to_string(m_x3));
+        m_entries[p3_index + 3]->set_sensitive(false);
+        enable_out_event = false;
+    }
+    if (m_y3 - m_y2 == 0.0) {
+        m_entries[p3_index + 4]->set_text(std::to_string(m_y3));
+        m_entries[p3_index + 4]->set_sensitive(false);
+        enable_out_event = false;
+    }
+    if (enable_out_event) {
+        m_entries[p3_index + 3]->add_events(Gdk::FOCUS_CHANGE_MASK);
+        m_entries[p3_index + 3]->signal_focus_out_event().connect(sigc::mem_fun(*this,
+            &CoordBox::on_x_entry_focus_out_event));
+        m_entries[p3_index + 4]->add_events(Gdk::FOCUS_CHANGE_MASK);
+        m_entries[p3_index + 4]->signal_focus_out_event().connect(sigc::mem_fun(*this,
+            &CoordBox::on_y_entry_focus_out_event));
+    }
 }
 
 
@@ -109,4 +146,48 @@ bool CoordBox::entries_filled() const {
         }
     }
     return filled;
+}
+
+
+bool CoordBox::on_x_entry_focus_out_event(GdkEventFocus* /* event*/) {
+    int x5_entry_index = m_entries.size() - 6;
+    double x5, y5;
+    std::stringstream stream_x;
+
+    DEBUG_MSG("x entry focus out event!");
+    DEBUG_MSG("------------------------");
+    DEBUG_MSG("x5 entry index = " << x5_entry_index);
+
+    stream_x << m_entries[x5_entry_index]->get_text().raw();
+    if (stream_x.str().size() != 0) {
+        stream_x >> x5;
+        y5 = m_y3 + (m_y3 - m_y2) * (x5 - m_x3) / (m_x3 - m_x2);
+        DEBUG_MSG("p2 = (" << m_x2 << "," << m_y2 << ")");
+        DEBUG_MSG("p3 = (" << m_x3 << "," << m_y3 << ")");
+        DEBUG_MSG("p5 = (" << x5   << "," << y5   << ")");
+        m_entries[x5_entry_index + 1]->set_text(std::to_string(y5));
+    }
+    return true;
+}
+
+
+bool CoordBox::on_y_entry_focus_out_event(GdkEventFocus* /* event*/) {
+    int y5_entry_index = m_entries.size() - 5;
+    double x5, y5;
+    std::stringstream stream_y;
+
+    DEBUG_MSG("y entry focus out event!");
+    DEBUG_MSG("------------------------");
+    DEBUG_MSG("y5 entry index = " << y5_entry_index);
+
+    stream_y << m_entries[y5_entry_index]->get_text().raw();
+    if (stream_y.str().size() != 0) {
+        stream_y >> y5;
+        x5 = m_x3 + (m_x3 - m_x2) * (y5 - m_y3) / (m_y3 - m_y2);
+        DEBUG_MSG("p2 = (" << m_x2 << "," << m_y2 << ")");
+        DEBUG_MSG("p3 = (" << m_x3 << "," << m_y3 << ")");
+        DEBUG_MSG("p5 = (" << x5   << "," << y5   << ")");
+        m_entries[y5_entry_index - 1]->set_text(std::to_string(x5));
+    }
+    return true;
 }
